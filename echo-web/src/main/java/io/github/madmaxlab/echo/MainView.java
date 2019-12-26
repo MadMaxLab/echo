4cb682aebcc6a1d8167cba90f9743a426e6ac325
Mad Max <maxim.bukvich@protonmail.com>
2019-12-26 22:07:06 +0300

    Add icons and hardcoded demo dialog.

--- echo-web/src/main/java/io/github/madmaxlab/echo/MainView.java
+++ echo-web/src/main/java/io/github/madmaxlab/echo/MainView.java
@@ -42,22 +42,35 @@ public class MainView extends VerticalLayout {
         chat.setId("chat");
         chat.setLoading(false);
         List<Message> messages = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            messages.add( new Message("Hello World!", "", "DEREZA", false));
+        // This is hardcode needs only for demo purpose.
+        String yourName = "You";
+        String opponentName = "potapov.m";
+        for (int i = 0; i < 20; i++) {
+            messages.add( new Message("Привет! Как дела?", "/icons/avatar1.png", opponentName, false));
         }
-
         chat.setMessages(messages);
+        chat.addNewMessage( new Message("Привет! Как дела?", "/icons/avatar1.png", opponentName, false));
+        chat.addNewMessage( new Message("Привет! Нормально, как сам?", "/icons/avatar2.png", yourName, true));
+        chat.addNewMessage( new Message("Да вот, латынь учу. Смотри что получилось пока освоить.", "/icons/avatar1.png", opponentName, false));
+        chat.addNewMessage( new Message("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "/icons/avatar1.png", opponentName, false));
+        chat.addNewMessage( new Message("Круто, молодец!", "/icons/avatar2.png", yourName, true));
+        chat.addNewMessage( new Message("А я новую фишку для нашего чата сделал! Теперь у нас отображаюстся аватарки пользователей!", "/icons/avatar2.png", yourName, true));
+        chat.addNewMessage( new Message("Угу, вижу. А свое изображение выбрать можно?", "/icons/avatar1.png", opponentName, false));
+        chat.addNewMessage( new Message("Конечно!", "/icons/avatar2.png", yourName, true));
+        chat.addNewMessage( new Message("Прекрасно! Спасибо!", "/icons/avatar1.png", opponentName, false));
         chat.scrollToBottom();
         chat.addChatNewMessageListener(event -> {
-            chat.addNewMessage(new Message(event.getMessage(), "", "Mad Max", true));
+            chat.addNewMessage(new Message(event.getMessage(), "/icons/avatar2.png", yourName, true));
             chat.clearInput();
+            chat.scrollToBottom();
         });
         chat.addLazyLoadTriggerEvent(event -> {
 
         });
         hLayout.add(chat);
         ListBox<String> contactListBox = new ListBox<>();
-        contactListBox.setItems("DEREZA", "YAGOZA", "Vasyliy Petrovich", "Petr Abramovich");
+        contactListBox.setItems("Потапов Михаил", "Иванов Иван", "Денисов Петр", "Петров Мансур");
+        contactListBox.setValue("Потапов Михаил");
         hLayout.add(contactListBox);
         hLayout.setWidthFull();
         add(hLayout);
